8469b8c0e26aa13f2ce28c2d4a36b989f45b8d60
CK-Langat <98855344+CK-Langat@users.noreply.github.com>
2022-12-19 17:35:10 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 
-
+//BEGINING OF OUTPUTSTYLE CODE//
 
 // const history = document.getElementById('history');
 const input = document.getElementById('input1');
@@ -299,5 +299,90 @@ positioninput.classList.add('noCaret')
 positioninput1.classList.add('noCaret')
 
 
+//END OF OUTPUTSTYLE CODE//
 
+
+
+
+
+
+//BEGINING OF AUTO SCROLL CODE//
+
+
+const acceptChild = document.getElementById("acceptchild")
+const allow1 = document.getElementById("allow1")
+const select = document.getElementById("allow2")
+const gearbutton = document.getElementById("gearbutton")
+const logo = document.getElementById("logo")
+
+let scrollable = true
+
+
+allow1.addEventListener("click", function () {
+
+    acceptChild.scrollIntoView({ block: "start" });
+    disableScroll()
+
+})
+
+gearbutton.addEventListener("click", function () {
+
+    logo.scrollIntoView({ behaviour: "smooth", block: "start" });
+    if (scrollable) {
+        disableScroll()
+    } else {
+        enableScroll()
+    }
+    scrollable = !scrollable
+
+})
+
+
+select.addEventListener('click', enableScroll)
+
+
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+    }));
+} catch (e) { }
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+//END OF AUTO SCROLL CODE//
 
